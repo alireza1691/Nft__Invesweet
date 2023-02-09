0ff266849be320ae8388f3863242fa52a1571911
Alireza <alirezashadow52555@gmail.com>
2023-02-09 12:39:33 +0330

    ش

--- contracts/CollectionCreator.sol
+++ contracts/CollectionCreator.sol
@@ -4,13 +4,14 @@ pragma solidity ^0.8.17;
 
 import "./CollectionV2.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 // import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 // import "./Collection.sol";
 // import "./CollectionV2.sol";
 
 error CollectionCreator__NotFound();
 error CollectionCreator__TransactionFailed();
-error CollectionCreator__BalanceZero();
+error CollectionCreator__InsufficientAmount();
 
 contract CollectionCreator{
 
@@ -18,10 +19,13 @@ contract CollectionCreator{
     event NewCollection(address requestCollectionFrom, address contractAddress);
     event Name();
 
-address immutable private owner;
+address immutable private i_owner;
+uint256 private s_createFee;
+AggregatorV3Interface internal immutable aggregator;
 
-constructor(){
-    owner = msg.sender;
+constructor(address aggregatorAddress){
+    i_owner = msg.sender;
+    aggregator = AggregatorV3Interface(aggregatorAddress);
 }
 
 mapping (address => address[]) private ownerToCollection;
@@ -31,7 +35,10 @@ function createSingleNft () external {}
 
 function createCollection(
     uint256 _price, uint256 _maxSupply, string memory _name, string memory _symbol, address _requsetFrom, string memory _uri
-    ) external {
+    ) external payable  {
+        if (msg.value < s_createFee) {
+            revert CollectionCreator__InsufficientAmount();
+        }
         CollectionV2 newNft = new CollectionV2(_name,_symbol,_price,_maxSupply,_requsetFrom, _uri);
         address _contractAddress = address(newNft);
         ownerToCollection[_requsetFrom].push(_contractAddress);
@@ -40,12 +47,18 @@ function createCollection(
 
 function mint(address collectionAddress) payable external {
     CollectionV2(collectionAddress)._mintWithEther(msg.sender,msg.value);
+    // (bool success,/* bytes memory data*/) = collectionAddress.delegatecall(
+    //         abi.encodeWithSignature("_mintWithEther(address, uint256)", msg.sender,msg.value)
+    //     );
+    // if (success == false) {
+    //     revert CollectionCreator__TransactionFailed();
+    // }
     balances[CollectionV2(collectionAddress).collectionOwner()] += msg.value;
 }
 
 function withdraw() external payable {
     if (balances[msg.sender] == 0) {
-        revert CollectionCreator__BalanceZero();
+        revert CollectionCreator__InsufficientAmount();
     }
     (bool ok,) = msg.sender.call{value: (balances[msg.sender] * 995 / 1000)}("");
     if (ok == false) {
--- contracts/CollectionV2.sol
+++ contracts/CollectionV2.sol
@@ -4,23 +4,24 @@ pragma solidity ^0.8.17;
 
 // import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 error Collection__NotEnoughValue();
 error Collection__MaximumSupply();
 error Collection__NotAuthorized();
 
-contract CollectionV2 is ERC721{
+contract CollectionV2 is  ERC721URIStorage{
 
 address immutable private i_creator;
 address immutable private i_owner;
 uint256 private s_counter = 0;
-uint256 immutable private i_price;
+uint256 immutable private i_mintFee;
 uint256 immutable private i_maxSupply;
 string private i_uri;
 
-constructor(string memory _name, string memory _symbol, uint256 _price ,uint256 _maxSupply, address _owner, string memory _uri) ERC721(_name,_symbol)  {
+constructor(string memory _name, string memory _symbol, uint256 _mintFee ,uint256 _maxSupply, address _owner, string memory _uri) ERC721(_name,_symbol)  {
     i_creator = msg.sender;
-    i_price = _price;
+    i_mintFee = _mintFee;
     i_maxSupply = _maxSupply;
     i_owner = _owner;
     i_uri = _uri;
@@ -46,26 +47,30 @@ modifier onlyOwner() {
 }
 
 function _mintWithEther (address requestFrom , uint256 amount) public onlyCreator{
-    if (amount <= i_price) {
+    if (amount <= i_mintFee) {
         revert Collection__NotEnoughValue();
     }
     if (i_maxSupply > 0 && s_counter > i_maxSupply) {
         revert Collection__MaximumSupply();
     }
  _safeMint(requestFrom, s_counter);
+ _setTokenURI(s_counter,i_uri);
  s_counter ++;
 }
-
 // function _mintWithUsd () external {
 // }
 
+function tokenURI(uint256 tokenId) public view override returns (string memory) {
+    // require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+    return i_uri;
+}
 
 function changeUri (string memory newUri) external onlyOwner{
     i_uri = newUri;
 }
 
 function price () external view returns(uint256){
-    return i_price;
+    return i_mintFee;
 }
 
 function maxSupply() external view returns(uint256){
