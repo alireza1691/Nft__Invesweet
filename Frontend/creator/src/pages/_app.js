dfab49b29d77eb9ba9a7ad07e45fb873787e663f
Alireza <alirezashadow52555@gmail.com>
2023-02-12 14:48:13 +0330

    address update

--- Frontend/creator/src/pages/_app.js
+++ Frontend/creator/src/pages/_app.js
@@ -1,8 +1,10 @@
 import '@/styles/globals.css'
 import Header from "../../components/Header"
 import Main from "../pages/index"
-import { MoralisProvider } from "react-moralis"
-import { NotificationProvider } from "web3uikit"
+// import { MoralisProvider } from "react-moralis"
+// import { NotificationProvider } from "web3uikit"
+
+// let {connectWallethandler, isConnected} = Main
 
 const APP_ID = process.env.NEXT_PUBLIC_APP_ID
 const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
@@ -10,10 +12,9 @@ const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
 export default function App({ Component, pageProps }) {
   return(
     <>
-    <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
-    <Header/>
+    {/* <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}> */}
       <Component {...pageProps}/>
-    </MoralisProvider>
+    {/* </MoralisProvider> */}
     </>
     )
 }
