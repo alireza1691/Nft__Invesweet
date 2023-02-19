7f95d3d215453d06a6188b5a1ec31d4f63d899f4
Alireza <alirezashadow52555@gmail.com>
2023-02-19 21:42:58 +0330

    ERC20V2

--- contracts/ERC20Creator.sol
+++ contracts/ERC20Creator.sol
@@ -8,13 +8,17 @@ import "./ERC20v2.sol";
 
 contract ERC20Creator {
 
+    event NewERC20(address indexed owner, address indexed tokenAddress,string name, string indexed symbol);
+
     address private immutable owner;
 
     constructor() {
         owner = msg.sender;
     }
 
-    function create( string memory name, string memory symbol, uint256 maxSupply, uint256 firstSupply , uint256 mineReward,uint256 decimals) external {
-        ERC20v2 newToken = new ERC20v2(name,symbol,maxSupply,firstSupply,mineReward,decimals,msg.sender);
+    function create( string memory name, string memory symbol, uint256 maxCap, uint256 firstSupply,uint256 burnPercent ,uint256 decimals) external {
+        ERC20v2 newToken = new ERC20v2(name,symbol,maxCap,firstSupply,burnPercent,decimals,msg.sender);
+        address newTokenAddress = address(newToken);
+        emit NewERC20(msg.sender, newTokenAddress, name, symbol);
     }
 }
--- contracts/ERC20v2.sol
+++ contracts/ERC20v2.sol
@@ -3,10 +3,11 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 
 
-contract ERC20v2 is ERC20 {
+contract ERC20v2 is ERC20 ,ERC20Burnable{
     mapping(address => uint256) private _balances;
 
     mapping(address => mapping(address => uint256)) private _allowances;
@@ -29,6 +30,11 @@ contract ERC20v2 is ERC20 {
         _maxSupply = maxSupply;
     }
 
+    modifier onlyOwner {
+        require(msg.sender == i_owner);
+        _;
+    }
+
     modifier supplyLimit (uint256 amount){
         if (_maxSupply > 0 ) {
             require (_maxSupply >= _totalSupply + amount,"Max Supply");
@@ -37,20 +43,20 @@ contract ERC20v2 is ERC20 {
        
     }
 
-    function mintByOwner(address userAddress, uint256 amount) external onlyOwner{
+    function mint(address userAddress, uint256 amount) external onlyOwner{
         _mint(userAddress, amount);
     }
 
-    function _beforeTokenTransfer(address from, uint256 amount) internal  /*returns(uint256)*/{
-        uint256 burnedAmount = amount * ((1000 - s_burnPercent)/1000);
-        unchecked {
-            _balances[from] = burnedAmount;
-            // Overflow not possible: amount <= accountBalance <= totalSupply.
-            _totalSupply -= burnedAmount;
-        }
+    // function _beforeTokenTransfer(address from, uint256 amount) internal  /*returns(uint256)*/{
+    //     uint256 burnedAmount = amount * ((1000 - s_burnPercent)/1000);
+    //     unchecked {
+    //         _balances[from] = burnedAmount;
+    //         // Overflow not possible: amount <= accountBalance <= totalSupply.
+    //         _totalSupply -= burnedAmount;
+    //     }
         
        
-    }
+    // }
 
    function _transfer(
         address from,
@@ -60,15 +66,24 @@ contract ERC20v2 is ERC20 {
         require(from != address(0), "ERC20: transfer from the zero address");
         require(to != address(0), "ERC20: transfer to the zero address");
 
-        _beforeTokenTransfer(from, amount);
-
         uint256 fromBalance = _balances[from];
         require(fromBalance >= amount, "ERC20: transfer amount exceeds balance");
+
+        uint256 finalAmount = amount;
+        if (s_burnPercent > 0) {
+            uint256 burnedAmount = (s_burnPercent * amount);
+            finalAmount -= burnedAmount;
+            unchecked {
+                _balances[address(0)] -= burnedAmount;
+                _totalSupply -= burnedAmount;
+            }
+        }
+
         unchecked {
             _balances[from] = fromBalance - amount;
             // Overflow not possible: the sum of all balances is capped by totalSupply, and the sum is preserved by
             // decrementing then incrementing.
-            _balances[to] += amount;
+            _balances[to] += finalAmount;
         }
 
         emit Transfer(from, to, amount);
@@ -79,7 +94,7 @@ contract ERC20v2 is ERC20 {
     function _mint(address account, uint256 amount) internal override supplyLimit(amount) {
         require(account != address(0), "ERC20: mint to the zero address");
 
-        _beforeTokenTransfer(address(0), account, amount);
+        super._beforeTokenTransfer(address(0), account, amount);
 
         _totalSupply += amount;
         unchecked {
@@ -91,15 +106,29 @@ contract ERC20v2 is ERC20 {
         _afterTokenTransfer(address(0), account, amount);
     }
 
+    function _beforeTokenTransfer(address from,address to,uint256 amount) internal virtual override{
+        if (s_burnPercent > 0) {
+            uint256 fromBalance = _balances[from];
+            uint256 burnedAmount = (s_burnPercent * amount);
+            unchecked {
+                _balances[from] = fromBalance - burnedAmount;
+                _balances[address(0)] -= burnedAmount;
+                _totalSupply -= burnedAmount;
+            }
+        }
+    }
+
     function setBurnPercent(uint256 amount) external onlyOwner{
         s_burnPercent = amount;
     }
+
     function burnByOwner( uint256 amount) external onlyOwner{
         _burn(msg.sender, amount);
     }
 
-    modifier onlyOwner {
-        require(msg.sender == i_owner);
-        _;
+    function destroy () external onlyOwner {
+        selfdestruct(payable(i_owner));
     }
+
+ 
 }
