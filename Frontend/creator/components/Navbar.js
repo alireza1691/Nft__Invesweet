c1b558b46e166c417536feaa912bb119c931f4dd
Alireza <alirezashadow52555@gmail.com>
2023-04-07 11:37:45 +0330

    button & tabs

--- Frontend/creator/components/Navbar.js
+++ Frontend/creator/components/Navbar.js
@@ -11,10 +11,16 @@ export default function Navbar() {
         {/* <img src='' alt=''/> */}
         <Image src="/logo.png" width={128} height={128} alt="Is loading"/>
     </div>
-    <Link href='/'style={{"marginLeft":"12px","marginRight":"60px","fontSize":"20px","color":"black"}} >Home</Link>
-    <Link href='/' style={{"marginLeft":"12px","marginRight":"60px"}} >About</Link>
-    <Link href='/' style={{"marginLeft":"12px","marginRight":"60px"}} >Orders</Link>
-    <ConnectButton></ConnectButton>
+    {/*  */}
+    <div className='dropDown'>
+      {/* ,"fontSize":"20px","color":"black","backgroundColor":"#EBEBDF","fontStyle":"normal","border":"solid","borderColor":"#DDDDCF","padding":"10px 10px","textDecorationLine":"blink" */}
+    <Link href='/'style={{"marginLeft":"12px","marginRight":"60px", "color": "#467889" ,"fontSize":"20px","textDecorationLine":"blink","padding":"10px 20px"}} >Home</Link>
+    <Link href='/' style={{"marginLeft":"12px","marginRight":"60px", "color": "#467889" ,"fontSize":"20px","textDecorationLine":"blink","padding":"10px 20px"}} >About</Link>
+    <Link href='/' style={{"marginLeft":"12px","marginRight":"60px", "color": "#467889" ,"fontSize":"20px","textDecorationLine":"blink","padding":"10px 20px"}} >Orders</Link>
+    
+    </div>
+    <button style={{"marginBottom":"15px","marginLeft":"100px","width":"160px","height":"40px","fontFamily":"sans-serif","fontSize":"20px","fontWeight":"bold","borderRadius":"10px","color":"#467889","border":"none"}}>Connect</button>
+    {/* <ConnectButton ></ConnectButton> */}
     </nav>
   )
 }
