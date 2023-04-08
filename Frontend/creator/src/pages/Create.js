7a561b592fa096f37b34e16b5c0a0cffc7a9d124
Alireza <alirezashadow52555@gmail.com>
2023-04-08 12:19:57 +0330

    box

--- Frontend/creator/src/pages/Create.js
+++ Frontend/creator/src/pages/Create.js
@@ -3,7 +3,16 @@ import React from 'react'
 function Create() {
   return (
     <div className='boxCreate'>
-        <div>hello</div>
+        <div>
+            <h6>Create your NFTs</h6>
+            <input className='input' placeholder='Enter name...' style={{"color":"black","width":"50%","margin":"5px"}} ></input>
+            <input className='input' placeholder='Enter symbol...' style={{"color":"black","width":"50%","margin":"5px"}} ></input>
+            <input className='input' placeholder='Enter price...' style={{"color":"black","width":"50%","margin":"5px"}} ></input>
+            <input className='input' placeholder='Enter max supply...' style={{"color":"black","width":"50%","margin":"5px"}} ></input>
+            <div>
+            <button style={{"border":"solid","width":"80px","padding":"5px 10px","margin":"10px","borderRadius":"5px"}} >Submit</button>
+            </div>
+        </div>
         
     </div>
   )
