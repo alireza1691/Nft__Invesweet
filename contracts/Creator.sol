ad84e44420850ae1ade2ec56552fe490cebb0636
Alireza <alirezashadow52555@gmail.com>
2023-05-07 15:46:22 +0330

    proxy issue

--- contracts/Creator.sol
+++ contracts/Creator.sol
@@ -4,6 +4,58 @@ pragma solidity ^0.8.17;
 // import "./ERC721Upgreadable.sol";
 import "./ERC721V1.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+// import "./Storage.sol";
+
+
+
+contract Storage is Ownable{
+
+    address private owner;
+    bytes32 public constant SLOT = keccak256("Invesweet");
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function changeOwnership (address newOwner) external onlyOwner {
+        owner = newOwner;
+    }
+
+    function getMintFee() external view returns(uint256){
+        return Fees.fee(SLOT).mintFee;
+    }
+    function getDeployFee() external view returns(uint256){
+        return Fees.fee(SLOT).mintFee;
+    }
+    function getWithdrawalFee() external view returns(uint256){
+        return Fees.fee(SLOT).mintFee;
+    }
+
+    function setMintFee(uint256 newAmount) external onlyOwner {
+        Fees.fee(SLOT).mintFee = newAmount;
+    }
+    function setDeployFee(uint256 newAmount) external onlyOwner {
+        Fees.fee(SLOT).deployFee = newAmount;
+    }
+    function setWithdrawalFee(uint256 newAmount) external onlyOwner {
+        Fees.fee(SLOT).withdrawalFee = newAmount;
+    }
+}
+
+library Fees{
+    struct FuncFees {
+        uint256 mintFee;
+        uint256 deployFee;
+        uint256 withdrawalFee;
+    }
+
+    function fee(bytes32 slot) internal pure returns(FuncFees storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+}
+
+
 
 
 contract Creator is Ownable {
@@ -11,7 +63,7 @@ contract Creator is Ownable {
     event ERC721Create(address indexed contractAddress, address indexed owner, string symbol);
 
     address private s_owner;
-    uint256 private s_ERC721Fee;
+    // uint256 private s_ERC721Fee;xw
 
     mapping (address => uint256) private balances;
     mapping (address => address[]) private addressToContracts;
@@ -22,7 +74,7 @@ contract Creator is Ownable {
 
 
     function createERC721(string memory name, string memory symbol, uint256 mintFee, uint256 maxSupply, string memory imageURL) payable external returns(address) {
-        require(msg.value >= s_ERC721Fee, "create exceeds fee");
+        require(msg.value >= Storage(address(this)).getDeployFee(), "create exceeds fee");
         ERC721V1 newNft = new ERC721V1(name, symbol ,mintFee, maxSupply, msg.sender, imageURL);
         emit ERC721Create(address(newNft), msg.sender, symbol);
         // contractToOwner[address(newNft)] = msg.sender;
@@ -49,18 +101,18 @@ contract Creator is Ownable {
         balances[owner] += (msg.value*99/100);
     }
 
-    function setFee(uint256 newFee) external onlyOwner {
-        s_ERC721Fee = newFee;
-    }
-
     // Getter funcs:
-
-    function getFee() external view returns(uint256) {
-        return s_ERC721Fee;
-    }
-
     function getUserBalance (address who) external view returns(uint256) {
         return balances[who];
+    }
+
+    function withdraw(uint256 amount) external payable {
+        if (balances[msg.sender] >= amount) {
+            uint256 withdrawable = (amount*95)/100;
+            balances[msg.sender] -= amount;
+            (bool ok,) = _msgSender().call{value: withdrawable }("");
+            require(ok,"Call failed");
+        }
     }
 
     receive() external payable {}
---
+++ contracts/Storage.sol
@@ -0,0 +1,60 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title A title that should describe the contract/interface
+/// @author The name of the author
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
+
+
+
+contract Storage is Ownable{
+
+    address private owner;
+    bytes32 public constant SLOT = keccak256("Invesweet");
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function changeOwnership (address newOwner) external onlyOwner {
+        owner = newOwner;
+    }
+
+    function getMintFee() external view returns(uint256){
+        return Fees.fee(SLOT).mintFee;
+    }
+    function getDeployFee() external view returns(uint256){
+        return Fees.fee(SLOT).mintFee;
+    }
+    function getWithdrawalFee() external view returns(uint256){
+        return Fees.fee(SLOT).mintFee;
+    }
+
+    function setMintFee(uint256 newAmount) external onlyOwner {
+        Fees.fee(SLOT).mintFee = newAmount;
+    }
+    function setDeployFee(uint256 newAmount) external onlyOwner {
+        Fees.fee(SLOT).deployFee = newAmount;
+    }
+    function setWithdrawalFee(uint256 newAmount) external onlyOwner {
+        Fees.fee(SLOT).withdrawalFee = newAmount;
+    }
+}
+
+library Fees{
+    struct FuncFees {
+        uint256 mintFee;
+        uint256 deployFee;
+        uint256 withdrawalFee;
+    }
+
+    function fee(bytes32 slot) internal pure returns(FuncFees storage r) {
+        assembly {
+            r.slot := slot
+        }
+    }
+}
+
