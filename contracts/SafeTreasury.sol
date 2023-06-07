7be5977c4e1265003e5faf208ecdfe9f3e3cd81e
Alireza <alirezashadow52555@gmail.com>
2023-06-07 22:58:54 +0330

    safe treasury native token

--- contracts/SafeTreasury.sol
+++ contracts/SafeTreasury.sol
@@ -4,32 +4,26 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-/// @title A title that should describe the contract/interface
-/// @author The name of the author
-/// @notice Explain to an end user what this does
-/// @dev Explain to a developer any extra details
-
-
-contract SafeTreasury is Ownable{
-
-    /* What we want?
-    Function to deposit
-    Function to withdraw
-    Function to get permit for specific token to specific address
-    Function to withdraw permtied amount of token
-    Function to authorize another address that can withdraw all funds
-    Function to remove authorize address
+/// @title A smart contract that works as a treasury to keep user assets safe.
+/// @author Alireza Haghshenas Github: alireza1691
+/// @notice This smart contract was written as a first version to test and develop and its not recommended to deploy on mainnet chains.
 
 
-    */
+contract SafeTreasury is Ownable , ReentrancyGuard{
+
+    error SafeTreasury__NotAuthorized();
+    error SafeTreasury__InsufficientBalance();
+    error SafeTreasury__TransactionFailed();
+    error SafeTreasury__AddressNotFound();
 
 // Mappings:
 
 // Mapping to get the address who authorized another address. We need this when an authorzied address wants to withdraw or transfer funcds of another address.
-mapping (address => address) authorizedAddressesToMainAddress;
+mapping (address => address) private authorizedAddressesToMainAddress;
 // Mapping to get token balance of each address (token address => user address => balance)
-mapping (address => mapping(address => uint256)) tokenAddressToOwnerToBalance;
+mapping (address => mapping(address => uint256)) private tokenAddressToOwnerToBalance;
 
 
 // External functions:
@@ -38,12 +32,15 @@ mapping (address => mapping(address => uint256)) tokenAddressToOwnerToBalance;
 /// @dev Before calling this function, to transfer ERC20 token in smart contract we need to approve the amount that smart contract can spend token.
 function deposit(address tokenAddress, uint256 amount) external {
     (bool ok) = IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount);
-    require(ok,"Transaction failed");
+    if (!ok) {
+        revert SafeTreasury__TransactionFailed();
+    }
     tokenAddressToOwnerToBalance[tokenAddress][msg.sender] += amount;
 }
 
 // Deposit native toke of the chain and the amount is msg.value.
 // NT stands for: native token
+// Note that here we assume address of native token as address(0)
 function depositNT() external payable {
     tokenAddressToOwnerToBalance[address(0)][msg.sender] += msg.value;
 }
@@ -51,48 +48,74 @@ function depositNT() external payable {
 
 // Withdrawal of assets if amount less than balance or equal.
 function withdraw (address tokenAddress, uint256 amount) external{
-    require(tokenAddressToOwnerToBalance[tokenAddress][msg.sender] >= amount,"Insufficient balance");
+    if (tokenAddressToOwnerToBalance[tokenAddress][msg.sender] < amount) {
+        revert SafeTreasury__InsufficientBalance();
+    }
     tokenAddressToOwnerToBalance[tokenAddress][msg.sender] -= amount;
     (bool ok) = IERC20(tokenAddress).transfer(msg.sender, amount);
-    require(ok,"Withdraw failed");
+    if (!ok) {
+        revert SafeTreasury__TransactionFailed();
+    }
 }
 
 // Withdrawal of native token of the current chain.
-// This withdrawal is same as previous one but we set token address as
-function withdrawNT(uint256 amount) external payable {
-    require(tokenAddressToOwnerToBalance[address(0)][msg.sender] > amount,"Insufficient balance");
+// This withdrawal is the same as the previous one but to withdraw native token we should call this function instead of the previous one.
+function withdrawNT(uint256 amount) external payable nonReentrant{
+    if (tokenAddressToOwnerToBalance[address(0)][msg.sender] < amount) {
+        revert SafeTreasury__InsufficientBalance();
+    }
     tokenAddressToOwnerToBalance[address(0)][msg.sender] -= amount;
     (bool ok,) = msg.sender.call{value: amount}("");
-    require(ok,"Withdraw failed");
+    if (!ok) {
+            revert SafeTreasury__TransactionFailed();
+        }
 }
 
 // Withdrawal just for addresses who authorized by another address and they have access to another address assets.
-// AD stands for: authorized address
-function withdrawByAD(address tokenAddress,uint256 amount) external {
-        address main = getAuthorized(msg.sender);
-        uint256 mainAddressBalance = tokenAddressToOwnerToBalance[tokenAddress][main];
-        require (mainAddressBalance > amount,"Insufficient balance" );
-        tokenAddressToOwnerToBalance[tokenAddress][main] -= amount;
+// AA stands for: authorized address
+function withdrawByAA(address tokenAddress,uint256 amount) external nonReentrant {
+        address mainAddress = getAuthorized(msg.sender);
+        if (mainAddress == address(0)) {
+            revert SafeTreasury__NotAuthorized(); 
+        }
+        uint256 mainAddressBalance = tokenAddressToOwnerToBalance[tokenAddress][mainAddress];
+        if (mainAddressBalance < amount) {
+            revert SafeTreasury__InsufficientBalance();
+        }
+        tokenAddressToOwnerToBalance[tokenAddress][mainAddress] -= amount;
         (bool ok) = IERC20(tokenAddress).transfer(msg.sender, amount);
-        require(ok,"Withdraw failed");
+        if (!ok) {
+            revert SafeTreasury__TransactionFailed();
+        }
 }
 
 // Same as previous one this function will withdraw user balance but this one uses for withdraw native token of chain.
-function withdrawNTByAD(uint256 amount) external {
-        address main = getAuthorized(msg.sender);
-        uint256 mainAddressBalance = tokenAddressToOwnerToBalance[address(0)][main];
-        require (mainAddressBalance > amount,"Insufficient balance" );
-        tokenAddressToOwnerToBalance[address(0)][main] -= amount;
+function withdrawNTByAA(uint256 amount) external nonReentrant {
+        address mainAddress = getAuthorized(msg.sender);
+        if (mainAddress == address(0)) {
+            revert SafeTreasury__NotAuthorized(); 
+        }
+        uint256 mainAddressBalance = tokenAddressToOwnerToBalance[address(0)][mainAddress];
+        if (mainAddressBalance < amount) {
+            revert SafeTreasury__InsufficientBalance();
+        }
+        tokenAddressToOwnerToBalance[address(0)][mainAddress] -= amount;
         (bool ok, ) = msg.sender.call{value: (amount*995)/1000 }("");
-        require(ok,"Withdraw failed");
+        if (!ok) {
+            revert SafeTreasury__TransactionFailed();
+        }
 }
 
 // Transfer asset from this smart contract to another address
-function externalTransfer(address tokenAddress, address to, uint256 amount) external{
-    require(tokenAddressToOwnerToBalance[tokenAddress][msg.sender] >= amount, "Insufficient balance");
+function externalTransfer(address tokenAddress, address to, uint256 amount) external nonReentrant{
+    if (tokenAddressToOwnerToBalance[tokenAddress][msg.sender] < amount) {
+        revert SafeTreasury__InsufficientBalance();
+    }
     tokenAddressToOwnerToBalance[tokenAddress][msg.sender] -= amount;
     bool ok = IERC20(tokenAddress).transfer(to, (amount * 995)/1000);
-    require(ok,"Transaction failed");
+    if (!ok) {
+        revert SafeTreasury__TransactionFailed();
+    }
 }
 
 // Transfer asset inside contract without withdrawing fund from this smart contract.
@@ -109,9 +132,14 @@ function authorize(address authorizedAddress) external {
 
 // This function will remove authorized address. 
 function removeAuthorize(address authorizedAddress) external {
-    require(authorizedAddressesToMainAddress[authorizedAddress] == msg.sender, "You have not any authorized address");
+    if (authorizedAddressesToMainAddress[authorizedAddress] != msg.sender) {
+        revert SafeTreasury__AddressNotFound();
+    }
         authorizedAddressesToMainAddress[authorizedAddress] = address(0);
 }
+
+// What else we need?
+// A function to withdraw fees
 
 // View funcs:
 
