9bfd50b6e54a0b54a116e764fbd14273396d0119
Alireza <alirezashadow52555@gmail.com>
2023-07-03 23:11:38 +0330

    ERC20V2

--- contracts/ERC20/ERC20v2.sol
+++ contracts/ERC20/ERC20v2.sol
@@ -1,124 +1,88 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.17;
+
+/// @title A title that should describe the contract/interface
+/// @author The name of the author
+/// @notice Explain to an end user what this does
+/// @dev Explain to a developer any extra details
+
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract MyToken is ERC20, Ownable {
+    address public marketingWallet;
+    address public constant BURN_ADDRESS = address(0);
+    uint256 public sellTaxRate = 10; // 1% tax, 0.5% to marketing wallet, 0.5% to rewards pool
+    uint256 public lastDistributeBurnTime;
+    uint256 public constant DISTRIBUTEBURN_TIMELOCK = 8 hours;
 
+    event MarketingWalletUpdated(address indexed newMarketingWallet);
+    event SellTaxRateUpdated(uint256 indexed newSellTaxRate);
+    event BurnDistributed(uint256 indexed amount);
 
-contract ERC20v2 is ERC20 ,ERC20Burnable{
-    mapping(address => uint256) private _balances;
-
-    mapping(address => mapping(address => uint256)) private _allowances;
-
-    uint256 immutable private i_decimals;
-    uint256 private _totalSupply;
-    string private _name;
-    string private _symbol;
-    address private immutable i_owner;
-    uint256 private s_burnPercent;
-    uint256 private  _maxSupply;
-
-    constructor(string memory name, string memory symbol, uint256 maxSupply, uint256 firstSupply, uint256 burnPercent, uint256 decimals ,address owner) ERC20(name,symbol)  {
-        _name = name;
-        _symbol = symbol;
-        _mint(msg.sender, firstSupply);
-        i_decimals = decimals;
-        i_owner = owner;
-        s_burnPercent = burnPercent;
-        _maxSupply = maxSupply;
+    /**
+     * @dev Constructor function that initializes the contract.
+     * @param _marketingWallet The address of the marketing wallet.
+     */
+    constructor(address _marketingWallet) ERC20("MyToken", "MTK") {
+        require(_marketingWallet!= address(0), "Invalid marketing wallet address");
+        marketingWallet = _marketingWallet;
+        uint256 initialSupply = 1000000 * (10 ** decimals()); // 1 million tokens with 18 decimal places
+        _mint(_msgSender(), initialSupply);
+        lastDistributeBurnTime = block.timestamp;
     }
 
-    modifier onlyOwner {
-        require(msg.sender == i_owner);
-        _;
-    }
-
-    modifier supplyLimit (uint256 amount){
-        if (_maxSupply > 0 ) {
-            require (_maxSupply >= _totalSupply + amount,"Max Supply");
+    /**
+     * @dev Overrides the _transfer function to apply taxes on non-marketing transfers.
+     * @param sender The address of the sender.
+     * @param recipient The address of the recipient.
+     * @param amount The amount to be transferred.
+     */
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual override {
+        if (recipient!= marketingWallet && sender!= marketingWallet && recipient!= BURN_ADDRESS && sender!= address(this)) {
+            uint256 taxAmount = amount * sellTaxRate / 1000;
+            uint256 marketingAmount = taxAmount / 2;
+            uint256 rewardsAmount = taxAmount - marketingAmount;
+            super._transfer(sender, marketingWallet, marketingAmount);
+            super._transfer(sender, address(this), rewardsAmount);
+            amount -= taxAmount;
         }
-        _;
-       
+        super._transfer(sender, recipient, amount);
     }
 
-    function mint(address userAddress, uint256 amount) external onlyOwner{
-        _mint(userAddress, amount);
-    }
-
-
-   function _transfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual override {
-        require(from != address(0), "ERC20: transfer from the zero address");
-        require(to != address(0), "ERC20: transfer to the zero address");
-
-        uint256 fromBalance = _balances[from];
-        require(fromBalance >= amount, "ERC20: transfer amount exceeds balance");
-
-        uint256 finalAmount = amount;
-        if (s_burnPercent > 0) {
-            uint256 burnedAmount = (s_burnPercent * amount);
-            finalAmount -= burnedAmount;
-            unchecked {
-                _balances[address(0)] -= burnedAmount;
-                _totalSupply -= burnedAmount;
-            }
+    /**
+     * @dev Function to distribute the burn. Can only be called once every 8 hours.
+     */
+    function distributeBurn() external {
+        require(block.timestamp >= lastDistributeBurnTime + DISTRIBUTEBURN_TIMELOCK, "Distribute burn is time locked");
+        uint256 balance = balanceOf(address(this));
+        if (balance > 0) {
+            _transfer(address(this), BURN_ADDRESS, balance);
+            emit BurnDistributed(balance);
         }
-
-        unchecked {
-            _balances[from] = fromBalance - amount;
-            // Overflow not possible: the sum of all balances is capped by totalSupply, and the sum is preserved by
-            // decrementing then incrementing.
-            _balances[to] += finalAmount;
-        }
-
-        emit Transfer(from, to, amount);
-
-        _afterTokenTransfer(from, to, amount);
+        lastDistributeBurnTime = block.timestamp;
     }
 
-    function _mint(address account, uint256 amount) internal virtual override supplyLimit(amount) {
-        require(account != address(0), "ERC20: mint to the zero address");
-
-        super._beforeTokenTransfer(address(0), account, amount);
-
-        _totalSupply += amount;
-        unchecked {
-            // Overflow not possible: balance + amount is at most totalSupply + amount, which is checked above.
-            _balances[account] += amount;
-        }
-        emit Transfer(address(0), account, amount);
-
-        _afterTokenTransfer(address(0), account, amount);
+    /**
+     * @dev Function to update the marketing wallet address. Can only be called by the owner.
+     * @param _marketingWallet The new marketing wallet address.
+     */
+    function setMarketingWallet(address _marketingWallet) external onlyOwner {
+        require(_marketingWallet!= address(0), "Invalid marketing wallet address");
+        marketingWallet = _marketingWallet;
+        emit MarketingWalletUpdated(_marketingWallet);
     }
 
-    function _beforeTokenTransfer(address from,address to,uint256 amount) internal virtual override{
-        if (s_burnPercent > 0) {
-            uint256 fromBalance = _balances[from];
-            uint256 burnedAmount = (s_burnPercent * amount);
-            unchecked {
-                _balances[from] = fromBalance - burnedAmount;
-                _balances[address(0)] -= burnedAmount;
-                _totalSupply -= burnedAmount;
-            }
-        }
+    /**
+     * @dev Function to update the sell tax rate. Can only be called by the owner.
+     * @param _sellTaxRate The new sell tax rate.
+     */
+    function setSellTaxRate(uint256 _sellTaxRate) external onlyOwner {
+        require(_sellTaxRate <= 100, "Invalid sell tax rate"); // Max 10% tax
+        sellTaxRate = _sellTaxRate;
+        emit SellTaxRateUpdated(_sellTaxRate);
     }
-
-    function setBurnPercent(uint256 amount) external onlyOwner{
-        s_burnPercent = amount;
-    }
-
-    function burnByOwner( uint256 amount) external onlyOwner{
-        _burn(msg.sender, amount);
-    }
-
-    function destroy () external onlyOwner {
-        selfdestruct(payable(i_owner));
-    }
-
- 
 }
